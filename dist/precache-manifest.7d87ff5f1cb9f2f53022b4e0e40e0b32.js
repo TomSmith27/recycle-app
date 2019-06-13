ccb72364e5259fee06b23bbe88de29d16873631c
Tom Smith <tom.smith2711@gmail.com>
2019-06-13 20:30:53 +0100

    deploy

---
+++ dist/precache-manifest.7d87ff5f1cb9f2f53022b4e0e40e0b32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "689af2bac26e127ddfa7",
+    "url": "/js/chunk-vendors.b980fa04.js"
+  },
+  {
+    "revision": "857c5b9e243532aaf997",
+    "url": "/js/app.28db2d7f.js"
+  },
+  {
+    "revision": "fcdb82cdc11c865fcc54",
+    "url": "/js/about.7f347ce7.js"
+  },
+  {
+    "revision": "d8d88800666e5ac3ff54d11418057e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "689af2bac26e127ddfa7",
+    "url": "/css/chunk-vendors.66440731.css"
+  },
+  {
+    "revision": "857c5b9e243532aaf997",
+    "url": "/css/app.ebd02902.css"
+  }
+];
